166ce9b04f15e2c3eca72615ba9988765273f2d1
Dinnyés Dávid <dinnyesd@scriptum.hu>
2023-06-12 14:35:06 +0200

    Példának megmutatni a mai konzultáláson

--- frontend-react/src/component/results/ResultTable.js
+++ frontend-react/src/component/results/ResultTable.js
@@ -1,6 +1,7 @@
+import {Link} from 'react-router-dom'
 
 const ResultTable = () => {
-	return <h1>Hello eredmények!</h1>
+	return <Link to={"https://primereact.org/datatable/"}><h1>Hello eredmények!</h1></Link>
 }
 
 export default ResultTable;
